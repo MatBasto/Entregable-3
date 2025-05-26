495229aed9de803a7a2bd274c70af529c2349167
MatBasto <mateobasto0106@hotmail.com>
2025-05-25 23:10:11 -0500

    Add files via upload

---
+++ Proyecto Final Web/src/App.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./Components/NavBar/NavBar";
+import AppRoutes from "./Routes/AppRoutes";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <AppRoutes />
+    </>
+  );
+}
+
+export default App;
